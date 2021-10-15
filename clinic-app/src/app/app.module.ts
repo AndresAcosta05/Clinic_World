ac54cf94806d2e1331f26db03c5a72cca0b8f55a
Andres_Acosta <andresdavidacostasarmiento@gmail.com>
2021-10-14 19:56:41 -0500

    pages

--- clinic-app/src/app/app.module.ts
+++ clinic-app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/components/header/header.component';
 import { FooterComponent } from './pages/components/footer/footer.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NosotrosComponent,
+    ContactenosComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
