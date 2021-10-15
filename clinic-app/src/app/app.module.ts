5c4b6a1294fc58b2ec07e58e4b63841d14d3b982
Andres_Acosta <andresdavidacostasarmiento@gmail.com>
2021-10-14 22:07:38 -0500

    paginas

--- clinic-app/src/app/app.module.ts
+++ clinic-app/src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './pages/components/header/header.component';
-import { FooterComponent } from './pages/components/footer/footer.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { ContactenosComponent } from './pages/contactenos/contactenos.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { NavegacionComponent } from './pages/components/navegacion/navegacion.component';
+import { HeaderComponent } from './pages/components/header/header.component';
+import { FooterComponent } from './pages/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     NosotrosComponent,
     ContactenosComponent,
-    InicioComponent
+    InicioComponent,
+    NavegacionComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
