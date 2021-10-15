dd55b308d35a42ad6e8095c228bd9094f2019af0
Carlo Quintero <fullarmy8@gmail.com>
2021-10-14 22:39:16 -0500

    rutas 1

--- clinic-app/src/app/app-routing.module.ts
+++ clinic-app/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
 import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 const routes: Routes = [
+  {path:'', component: InicioComponent},
   {path: 'contactenos', component: ContactenosComponent},
-  {path: 'nosotros', component: NosotrosComponent}
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'blog', component: BlogComponent}
 ];
 
 @NgModule({
